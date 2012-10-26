dc9458472f84d63d60afd5356ff7eda6c7bbe408
Victor Fernandez de Alba <victor.fernandez@upcnet.es>
2012-10-26 12:17:30 +0200

    Personal bar, first version

--- genweb/theme/browser/viewlets.py
+++ genweb/theme/browser/viewlets.py
@@ -1,15 +1,18 @@
 from five import grok
 from Acquisition import aq_inner
+from AccessControl import getSecurityManager
 from zope.interface import Interface
 from zope.component import getMultiAdapter
 from zope.app.component.hooks import getSite
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore import permissions
 
 from plone.app.layout.viewlets.common import PersonalBarViewlet
 from plone.app.layout.viewlets.interfaces import IPortalTop
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 
+from genweb.core.utils import havePermissionAtRoot, assignAltAcc
 from genweb.theme.browser.interfaces import IGenwebTheme
 
 
@@ -20,10 +23,13 @@ class viewletBase(grok.Viewlet):
     grok.baseclass()
 
     def portal_url(self):
-        self.portal_object.absolute_url()
+        self.portal().absolute_url()
 
-    def portal_object(self):
+    def portal(self):
         return getSite()
+
+    def assignAltAcc(self):
+        return assignAltAcc(self)
 
 
 class gwPersonalBarViewlet(PersonalBarViewlet, viewletBase):
@@ -32,3 +38,9 @@ class gwPersonalBarViewlet(PersonalBarViewlet, viewletBase):
     grok.layer(IGenwebTheme)
 
     index = ViewPageTemplateFile('viewlets_templates/personal_bar.pt')
+
+    def showRootFolderLink(self):
+        return havePermissionAtRoot(self)
+
+    def canManageSite(self):
+        return getSecurityManager().checkPermission("plone.app.controlpanel.Overview", self.portal)
