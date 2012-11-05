2f60a47bcf91269beb51128e51bbcbac54fb561c
Victor Fernandez de Alba <victor.fernandez@upcnet.es>
2012-11-05 19:39:27 +0100

    WIP multilingual views

--- genweb/theme/browser/viewlets.py
+++ genweb/theme/browser/viewlets.py
@@ -7,16 +7,18 @@ from zope.app.component.hooks import getSite
 
 from plone.memoize.view import memoize_contextless
 
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore import permissions
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from plone.app.layout.viewlets.common import PersonalBarViewlet, GlobalSectionsViewlet, PathBarViewlet
 from plone.app.layout.viewlets.common import ManagePortletsFallbackViewlet, ContentViewsViewlet
 from plone.app.layout.viewlets.interfaces import IPortalTop, IPortalHeader, IBelowContent
 from plone.app.layout.viewlets.interfaces import IPortalFooter
+from plone.app.i18n.locales.browser.selector import LanguageSelector
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 
-from genweb.core.utils import havePermissionAtRoot, assignAltAcc
+from genweb.core.utils import gw_config, havePermissionAtRoot, assignAltAcc
 from genweb.theme.browser.interfaces import IGenwebTheme
 
 
@@ -33,6 +35,15 @@ class viewletBase(grok.Viewlet):
     @memoize_contextless
     def portal(self):
         return getSite()
+
+    def gw_config(self):
+        return gw_config()
+
+    def pref_lang(self):
+        """Funcio que extreu idioma actiu
+        """
+        lt = getToolByName(self.portal(), 'portal_languages')
+        return lt.getPreferredLanguage()
 
     def assignAltAcc(self):
         return assignAltAcc(self)
@@ -52,11 +63,39 @@ class gwPersonalBarViewlet(PersonalBarViewlet, viewletBase):
         return getSecurityManager().checkPermission("plone.app.controlpanel.Overview", self.portal)
 
 
-class gwHeader(viewletBase):
+class gwHeader(LanguageSelector, viewletBase):
     grok.name('genweb.header')
     grok.template('header')
     grok.viewletmanager(IPortalHeader)
     grok.layer(IGenwebTheme)
+
+    def languages(self):
+        languages_info = super(LanguageSelectorViewlet, self).languages()
+        results = []
+        translation_group = queryAdapter(self.context, ITG)
+        if translation_group is None:
+            translation_group = NOTG
+        for lang_info in languages_info:
+            # Avoid to modify the original language dict
+            data = lang_info.copy()
+            data['translated'] = True
+            query_extras = {
+                'set_language': data['code'],
+            }
+            post_path = getPostPath(self.context, self.request)
+            if post_path:
+                query_extras['post_path'] = post_path
+            data['url'] = addQuery(
+                self.request,
+                self.context.absolute_url().rstrip("/") + \
+                    "/@@multilingual-selector/%s/%s" % (
+                        translation_group,
+                        lang_info['code']
+                    ),
+                **query_extras
+            )
+            results.append(data)
+        return results
 
 
 class gwGlobalSectionsViewlet(GlobalSectionsViewlet, viewletBase):
