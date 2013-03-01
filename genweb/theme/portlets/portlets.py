3e9f2e11d66a312f0373a4a7ae79b137972ed075
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-03-01 13:12:18 +0100

    Update relentless portlets

--- genweb/theme/portlets/portlets.py
+++ genweb/theme/portlets/portlets.py
@@ -1,5 +1,8 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from plone.app.portlets.portlets.navigation import Renderer as NavigationRenderer
+from plone.app.portlets.portlets.rss import Renderer as RssRenderer
+from plone.portlet.collection.collection import Renderer as CollectionRenderer
 
 
 class gwNavigation(NavigationRenderer):
@@ -8,3 +11,17 @@ class gwNavigation(NavigationRenderer):
     """
     _template = ViewPageTemplateFile('templates/navigation.pt')
     recurse = ViewPageTemplateFile('templates/navigation_recurse.pt')
+
+
+class gwRSS(RssRenderer):
+    """ The standard rss portlet override 'old style'
+        as it doesn't allow to do it jbot way...
+    """
+    render_full = ZopeTwoPageTemplateFile('templates/rss.pt')
+
+
+class gwCollection(CollectionRenderer):
+    """ The standard rss portlet override 'old style'
+        as it doesn't allow to do it jbot way...
+    """
+    _template = ViewPageTemplateFile('templates/collection.pt')
