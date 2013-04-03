10fdc54fc4cfa8b77e958578fe90f3a7b931b038
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-04-03 16:34:19 +0200

    Recaptcha from collective, and CSS fails.

--- genweb/theme/browser/views.py
+++ genweb/theme/browser/views.py
@@ -28,6 +28,7 @@ from scss import Scss
 from genweb.theme.scss import dynamic_scss
 
 from plone.formwidget.recaptcha.view import RecaptchaView, IRecaptchaInfo
+from collective.recaptcha.view import RecaptchaView as CollectiveRecaptchaView
 from recaptcha.client.captcha import displayhtml
 
 from Products.CMFPlone import PloneMessageFactory as _
@@ -358,6 +359,8 @@ class gwCatalogNavigationTabs(CatalogNavigationTabs):
 
 
 class gwRecaptchaView(RecaptchaView, grok.View):
+    """ Override of the original plone.formwidget.recaptcha view to match style
+        and language options """
     grok.context(Interface)
     grok.name('recaptcha')
     grok.require('zope2.Public')
@@ -387,7 +390,7 @@ class gwRecaptchaView(RecaptchaView, grok.View):
                                 };
                         </script>
                         """ % lang,
-                    "es": """
+                   "es": """
                         <script type="text/javascript">
                             var RecaptchaOptions = {
                                     lang : '%s',
@@ -395,7 +398,64 @@ class gwRecaptchaView(RecaptchaView, grok.View):
                             };
                         </script>
                         """ % lang,
-                    "en": """
+                   "en": """
+                        <script type="text/javascript">
+                            var RecaptchaOptions = {
+                                    lang : '%s',
+                                    theme : 'clean'
+                            };
+                        </script>
+                        """ % lang
+        }
+
+        if not self.settings.public_key:
+            raise ValueError('No recaptcha public key configured. Go to path/to/site/@@recaptcha-settings to configure.')
+        use_ssl = self.request['SERVER_URL'].startswith('https://')
+        error = IRecaptchaInfo(self.request).error
+        return options.get(lang, '') + displayhtml(self.settings.public_key, use_ssl=use_ssl, error=error)
+
+
+class gwCollectiveRecaptchaView(CollectiveRecaptchaView, grok.View):
+    """ Override of the original collective.recaptcha view to match style
+        and language options """
+    grok.context(Interface)
+    grok.name('captcha')
+    grok.require('zope2.Public')
+    grok.layer(IGenwebTheme)
+
+    def render(self):
+        pass
+
+    def image_tag(self):
+        lang = pref_lang()
+        options = {"ca": """
+                        <script type="text/javascript">
+                            var RecaptchaOptions = {
+                                    custom_translations : {
+                                            instructions_visual : "Escriu les dues paraules:",
+                                            instructions_audio : "Transcriu el que sentis:",
+                                            play_again : "Torna a escoltar l'\u00e0udio",
+                                            cant_hear_this : "Descarrega la pista en MP3",
+                                            visual_challenge : "Modalitat visual",
+                                            audio_challenge : "Modalitat auditiva",
+                                            refresh_btn : "Demana dues noves paraules",
+                                            help_btn : "Ajuda",
+                                            incorrect_try_again : "Incorrecte. Torna-ho a provar.",
+                                    },
+                                    lang : '%s',
+                                    theme : 'clean'
+                                };
+                        </script>
+                        """ % lang,
+                   "es": """
+                        <script type="text/javascript">
+                            var RecaptchaOptions = {
+                                    lang : '%s',
+                                    theme : 'clean'
+                            };
+                        </script>
+                        """ % lang,
+                   "en": """
                         <script type="text/javascript">
                             var RecaptchaOptions = {
                                     lang : '%s',
