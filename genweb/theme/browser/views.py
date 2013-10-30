c595e5efe13d28f2c50858f811f4791bc5d953e1
Corina Riba <corina.riba@upcnet.es>
2013-10-30 12:11:45 +0100

    Direccion from mensajes de envio eventos

--- genweb/theme/browser/views.py
+++ genweb/theme/browser/views.py
@@ -584,7 +584,6 @@ class gwSendEventView(grok.View):
         portal = urltool.getPortalObject()
         email_charset = portal.getProperty('email_charset')
         to_address = 'corina.riba@upcnet.es'
-        import ipdb;ipdb.set_trace()
         from_name = portal.getProperty('email_from_name')
         from_address = portal.getProperty('email_from_address')
         titulo_web = portal.getProperty('title')
