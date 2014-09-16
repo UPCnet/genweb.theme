0e0895f8178831053efd4ffe44d4dbbfd7572525
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-09-16 20:35:29 +0200

    Fix the remaining resources with fixed /++genweb++static/ paths

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ additional_import_paths = [
 http_path = "/"
 css_dir = "./genweb/theme/stylesheets"
 sass_dir = "./genweb/theme/scss"
-http_images_path = "/++genweb++static/images"
+http_images_path = "++genweb++static/images"
 images_dir = "../genweb.core/genweb/core/static/images"
 javascripts_dir = "./genweb/theme/bootstrap/js"
 
