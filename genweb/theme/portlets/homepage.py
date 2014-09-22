a1a369361df5a09ea8cd2e4af9cad73b3b5c402d
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-09-22 12:24:31 +0200

    Fix of benvingut portlet

--- genweb/theme/portlets/homepage.py
+++ genweb/theme/portlets/homepage.py
@@ -1,11 +1,8 @@
+from plone import api
 from zope.interface import implements
 
-from Products.CMFCore.utils import getToolByName
-from Acquisition import aq_inner
 from genweb.core.interfaces import IHomePage
 from genweb.core.utils import pref_lang
-from zope.component import getMultiAdapter
-
 
 from plone.portlets.interfaces import IPortletDataProvider
 from plone.app.portlets.portlets import base
@@ -33,11 +30,13 @@ class Renderer(base.Renderer):
 
     def getHomepage(self):
         page = {}
-        context = aq_inner(self.context)
-        pc = getToolByName(context, 'portal_catalog')
+        pc = api.portal.get_tool('portal_catalog')
         result = pc.searchResults(object_provides=IHomePage.__identifier__,
                                   Language=pref_lang())
-        page['body'] = result[0].CookedBody()
+        if not result:
+            page['body'] = ''
+        else:
+            page['body'] = result[0].getObject().text.output
 
         return page
 
