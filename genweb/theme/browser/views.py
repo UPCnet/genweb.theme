66ea10386d4a6d9a3925e292dcea324cb8c3e5ac
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-07-27 14:49:18 +0200

    Transferred recaptcha override to a patch in genweb.upc

--- genweb/theme/browser/views.py
+++ genweb/theme/browser/views.py
@@ -486,62 +486,62 @@ class gwLayoutPolicy(LayoutPolicy):
         return body_class
 
 
-class gwRecaptchaView(RecaptchaView, grok.View):
-    """ Override of the original plone.formwidget.recaptcha view to match style
-        and language options """
-    grok.context(Interface)
-    grok.name('recaptcha')
-    grok.require('zope2.Public')
-    grok.layer(IGenwebTheme)
+# class gwRecaptchaView(RecaptchaView, grok.View):
+#     """ Override of the original plone.formwidget.recaptcha view to match style
+#         and language options """
+#     grok.context(Interface)
+#     grok.name('recaptcha')
+#     grok.require('zope2.Public')
+#     grok.layer(IGenwebTheme)
 
-    def render(self):
-        pass
+#     def render(self):
+#         pass
 
-    def image_tag(self):
-        lang = pref_lang()
-        options = {"ca": """
-                            <script type="text/javascript">
-                                var RecaptchaOptions = {
-                                        custom_translations : {
-                                                instructions_visual : "Escriu les dues paraules:",
-                                                instructions_audio : "Transcriu el que sentis:",
-                                                play_again : "Torna a escoltar l'\u00e0udio",
-                                                cant_hear_this : "Descarrega la pista en MP3",
-                                                visual_challenge : "Modalitat visual",
-                                                audio_challenge : "Modalitat auditiva",
-                                                refresh_btn : "Demana dues noves paraules",
-                                                help_btn : "Ajuda",
-                                                incorrect_try_again : "Incorrecte. Torna-ho a provar.",
-                                        },
-                                        lang : '%s',
-                                        theme : 'clean'
-                                    };
-                            </script>
-                            """ % lang,
-                   "es": """
-                            <script type="text/javascript">
-                                var RecaptchaOptions = {
-                                        lang : '%s',
-                                        theme : 'clean'
-                                };
-                            </script>
-                            """ % lang,
-                   "en": """
-                            <script type="text/javascript">
-                                var RecaptchaOptions = {
-                                        lang : '%s',
-                                        theme : 'clean'
-                                };
-                            </script>
-                            """ % lang
-                   }
+#     def image_tag(self):
+#         lang = pref_lang()
+#         options = {"ca": """
+#                             <script type="text/javascript">
+#                                 var RecaptchaOptions = {
+#                                         custom_translations : {
+#                                                 instructions_visual : "Escriu les dues paraules:",
+#                                                 instructions_audio : "Transcriu el que sentis:",
+#                                                 play_again : "Torna a escoltar l'\u00e0udio",
+#                                                 cant_hear_this : "Descarrega la pista en MP3",
+#                                                 visual_challenge : "Modalitat visual",
+#                                                 audio_challenge : "Modalitat auditiva",
+#                                                 refresh_btn : "Demana dues noves paraules",
+#                                                 help_btn : "Ajuda",
+#                                                 incorrect_try_again : "Incorrecte. Torna-ho a provar.",
+#                                         },
+#                                         lang : '%s',
+#                                         theme : 'clean'
+#                                     };
+#                             </script>
+#                             """ % lang,
+#                    "es": """
+#                             <script type="text/javascript">
+#                                 var RecaptchaOptions = {
+#                                         lang : '%s',
+#                                         theme : 'clean'
+#                                 };
+#                             </script>
+#                             """ % lang,
+#                    "en": """
+#                             <script type="text/javascript">
+#                                 var RecaptchaOptions = {
+#                                         lang : '%s',
+#                                         theme : 'clean'
+#                                 };
+#                             </script>
+#                             """ % lang
+#                    }
 
-        if not self.settings.public_key:
-            raise ValueError('No recaptcha public key configured. Go to path/to/site/@@recaptcha-settings to configure.')
-        use_ssl = self.request['SERVER_URL'].startswith('https://')
-        error = IRecaptchaInfo(self.request).error
+#         if not self.settings.public_key:
+#             raise ValueError('No recaptcha public key configured. Go to path/to/site/@@recaptcha-settings to configure.')
+#         use_ssl = self.request['SERVER_URL'].startswith('https://')
+#         error = IRecaptchaInfo(self.request).error
 
-        return options.get(lang, '') + displayhtml(self.settings.public_key, use_ssl=use_ssl, error=error)
+#         return options.get(lang, '') + displayhtml(self.settings.public_key, use_ssl=use_ssl, error=error)
 
 
 class gwCollectiveRecaptchaView(CollectiveRecaptchaView, grok.View):
