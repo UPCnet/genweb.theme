c431204437a403994183953ef11025200220219d
sandra.saornil <sandra.saornil@upcnet.es>
2017-02-16 15:11:53 +0100

    Override default 'Content-Type' header for RSS feeds
    Products.PloneCMF syndication FeedView returns 'application/atom+xml'
    by default but browsers expect 'application/xml' instead.
    
    Expanded FeedView override, FeedViewCustom to replace default header value

--- genweb/theme/overrides.py
+++ genweb/theme/overrides.py
@@ -4,6 +4,11 @@ from DateTime.DateTime import DateTime
 
 class FeedViewCustom(FeedView):
 
+    def __call__(self):
+        return_value = super(FeedView, self).__call__()
+        self.request.response.setHeader('Content-Type', 'application/xml')
+        return return_value
+
     def localized_time(self, date):
         local_date = DateTime(date)
         return local_date.strftime('%Y-%m-%d %X')
