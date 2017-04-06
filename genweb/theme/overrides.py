8e6064355f29fe42764961959c3c444cec842c41
Iago López Fernández <iago.lopez@upcnet.es>
2017-04-06 12:21:38 +0200

    Codificar resultados del adaptador

--- genweb/theme/overrides.py
+++ genweb/theme/overrides.py
@@ -20,28 +20,3 @@ class FeedViewCustom(FeedView):
     def localized_time(self, date):
         local_date = DateTime(date)
         return local_date.strftime('%Y-%m-%d %X')
-
-
-class FormSaveDataAdapterCustom(FormSaveDataAdapter):
-
-    def _addDataRow(self, value):
-
-        self._migrateStorage()
-
-        if isinstance(self._inputStorage, IOBTree):
-            # 32-bit IOBTree; use a key which is more likely to conflict
-            # but which won't overflow the key's bits
-
-            id = self._inputItems
-            self._inputItems += 1
-        else:
-            # 64-bit LOBTree
-            id = int(time.time() * 1000)
-            while id in self._inputStorage:  # avoid collisions during testing
-                id += 1
-        self._inputStorage[id] = self.decodeCharacters(value)
-        self._length.change(1)
-
-    def decodeCharacters(self, txt):
-        txt[3] = txt[3].decode('string_escape')
-        return txt
